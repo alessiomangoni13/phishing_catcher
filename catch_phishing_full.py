22ff22158714fe32ad311401f52514d7e8e776a0
meng <alessiomangoni13@gmail.com>
2021-01-22 09:25:31 +0100

    Update catch_phishing_full.py
    updating certstream dict schema

--- catch_phishing_full.py
+++ catch_phishing_full.py
@@ -195,7 +195,7 @@ def callback(message, context):
             score = score_domain(domain.lower())
 
             # If issued from a free CA = more suspicious
-            if "Let's Encrypt" in message['data']['chain'][0]['subject']['aggregated']:
+            if "Let's Encrypt" in message["data"]["leaf_cert"]["issuer"]["aggregated"]:
                 score += 10
                 evaluating.debug('%s letsencrypt_CA added score:%s', domain, score,)
 
